80cbdac66717f20b3f1777d48416908682c5e7ac
ermongabriel <ermongabriel.tech@gmail.com>
2026-07-20 07:24:41 +0100

    fix: done with CI issues test 3

--- eslint.config.js
+++ eslint.config.js
@@ -18,5 +18,14 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
     },
+    rules: {
+      'react-refresh/only-export-components': 'off',
+      'react-hooks/set-state-in-effect': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
   },
 ])
